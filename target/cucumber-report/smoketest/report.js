9b92695894593f50462dc02237c0fdffd4dab0a9
epicallynoobish <37289763+epicallynoobish@users.noreply.github.com>
2018-04-05 10:26:22 +0500

    Add files via upload

--- target/cucumber-report/smoketest/report.js
+++ target/cucumber-report/smoketest/report.js
@@ -530,3 +530,51 @@ formatter.background({
   "description": "",
   "keyword": "Background"
 });
+formatter.step({
+  "name": "I log in as demo with password demo with name Иванов О.З.",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepDef.i_log_in_as_demo_with_password_demo_with_name_Иванов_О_З()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Test data existance outline",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@tag"
+    },
+    {
+      "name": "@tag2"
+    }
+  ]
+});
+formatter.step({
+  "name": "I go to \"http://eaistan-dev.proitr.ru/index.php?show\u003dKpgzSpgz\u0026menu_id\u003d1503\u0026report_id\u003d1507\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepDef.i_go_to(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "I check for the table",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "stepDef.i_check_for_the_table()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
